6598b169070933b0fdbc47fc93747f145450cafb
junu1229 <junu1229@gmail.com>
2023-06-01 15:00:12 +0900

    Update Test.java

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -5,6 +5,15 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int number = 10;
+		String name = "junu";
+		String name1 = "junu";
+		String name2 = "junu";
+		System.out.println(name1);
+		System.out.println(name1);
+		System.out.println(name1);
+		System.out.println(name1);
+		System.out.println(name1);
+		
 	}
 
 }
